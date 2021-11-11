4a76e653261ec15a9447672cf3b874d628fce4dc
Josué Munganga <jmunganga039@gmail.com>
2021-11-11 12:00:28 +0200

    Passing input label as a component children

--- src/App.js
+++ src/App.js
@@ -51,11 +51,13 @@ const App = () => {
 
       <InputWithLabel
         id="search"
-        label="Search"
         value={searchTerm} 
         onInputChange={handleSearch} 
-       
-      />
+      >
+        <strong>
+          Search:
+        </strong>
+      </InputWithLabel>
 
       <hr />
 
@@ -64,9 +66,9 @@ const App = () => {
   );
 };
 
-const InputWithLabel = ({ id, label, value, type = 'text', onInputChange }) => (
+const InputWithLabel = ({ id, value, type = 'text', onInputChange, children }) => (
   <>
-    <label htmlFor={id}>{label} </label>
+    <label htmlFor={id}>{children} </label>
     &nbsp;
     <input
       id={id}
